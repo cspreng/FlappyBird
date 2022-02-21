21d41b5d7dc59477ae0dfa14547ceeacfcfd87d0
Carlos Eduardo Spreng <carlos.spreng@gmail.com>
2022-02-20 23:10:06 -0300

    chao

--- FlappyBird.py
+++ FlappyBird.py
@@ -131,3 +131,27 @@ class Passaro:
                 return True
             else:
                 return False
+
+    class Chao:
+        VELOCIDADE = 5
+        LARGURA = IMAGEM_CHAO.get_width()
+        IMAGEM = IMAGEM_CHAO
+
+        def __init__(self, y):
+            self.y = y
+            self.x0 = 0
+            self.x1 = self.LARGURA
+
+        def mover(self):
+            self.x0 -= self.VELOCIDADE
+            self.x1 -= self.VELOCIDADE
+
+            if self.x0 + self.LARGURA < 0:
+                self.x0 = self.x0 + self.LARGURA
+            if self.x1 + self.LARGURA < 0:
+                self.x1 = self.x1 + self.LARGURA
+
+        def desenhar(self, tela):
+            tela.blit(self.IMAGEM, (self.x0, self.y))
+            tela.blit(self.IMAGEM, (self.x1, self.y))
+
